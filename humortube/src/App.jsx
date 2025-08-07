40e5e0ce0b07bc65bd5a3d16f72342cdb9d683be
kidujeong-hub <kidujeong@gmail.com>
2025-08-07 16:34:12 +0900

    유머

---
+++ humortube/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+
+  return (
+    <>
+      <div className="center_div">
+        <h1> 웃길 영상</h1>
+
+        <h2>쇼츠 모음</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/02yRdVWVWeY?si=a3u0z6lJ3SUzR2_R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <h2>무도1</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/VpxvOijmOAE?si=wlyQLiOER7bQJMeH&amp;start=29" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/XcUMqIv9W4M?si=hhZlj8Q_j7B5jO-0&amp;start=29" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <h2>아형</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/3VgX4-tYeYM?si=TZ7ccupSwd1gfk6j&amp;start=117" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
